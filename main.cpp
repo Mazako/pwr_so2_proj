c5f27a3f09a652450a8a48c7612b1eaf597c6386
michał <michal.maziarz12@gmail.com>
2024-05-17 18:08:06 +0200

    ncurses final?

--- main.cpp
+++ main.cpp
@@ -12,12 +12,71 @@
 // (D) - DOSTAWA
 // (S) - SPRZĘT
 
-void cursesDemo() {
-    initscr();
-    printw("Hello World! Press any key to exit ...");
-    refresh();
+void cursesDemo() {    // Inicjalizacja ncurses
+    initscr();             // Przełącz do trybu ncurses
+    cbreak();              // Linie wejściowe są przekazywane do bufora, bez buforowania
+    noecho();              // Nie wyświetlaj wpisywanych znaków
+    keypad(stdscr, TRUE);  // Włącz obsługę klawiszy funkcyjnych i strzałek
+
+    // Deklaracja opcji do wyświetlenia
+    std::vector<std::string> options = {"Opcja 1", "Opcja 2", "Opcja 3", "Wyjście"};
+
+    int choice;
+    int highlight = 0;
+
+    while (true) {
+        // Wypisywanie wszystkich opcji, wyróżnienie wybranej
+        for (int i = 0; i < options.size(); ++i) {
+            if (i == highlight) {
+                attron(A_REVERSE);  // Odwróć kolor dla wyróżnienia
+            }
+            mvprintw(i, 0, options[i].c_str());
+            if (i == highlight) {
+                attroff(A_REVERSE);
+            }
+        }
+
+        // Odczytanie klawisza od użytkownika
+        choice = getch();
+
+        // Poruszanie się po opcjach
+        switch (choice) {
+        case KEY_UP:
+            if (highlight > 0) {
+                --highlight;
+            }
+            break;
+        case KEY_DOWN:
+            if (highlight < options.size() - 1) {
+                ++highlight;
+            }
+            break;
+        default:
+            break;
+        }
+
+        // Jeśli wciśnięto Enter, zakończ pętlę
+        if (choice == 10) {  // 10 to kod klawisza Enter
+            break;
+        }
+    }
+
+    // Czyszczenie ekranu
+    clear();
+
+    // Wyświetlenie wybranej opcji
+    if (highlight == options.size() - 1) {
+        printw("Zakończenie programu...");
+    } else {
+        printw("Wybrano: %s", options[highlight].c_str());
+    }
+
+    // Oczekiwanie na naciśnięcie dowolnego klawisza przed wyjściem
     getch();
+
+    // Zakończenie pracy z ncurses
     endwin();
+
 }
 
 void simulation() {
